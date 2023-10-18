dc6efcdf54c5c37a921310376412f6e52c0d7bf3
Kiwicase <emailcaseydent@gmail.com>
2023-10-18 13:57:41 +0300

    Navbar logo and heading added

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -11,14 +11,14 @@ const Navbar = () => {
 
     return (
         <nav className="navbar">
+            <div className="navbar__logo-container">
+                <img src="https://res.cloudinary.com/dqoibnakh/image/upload/v1697624567/p8efqixdqmiqydahbl4x.png" alt="TRV Logo" className="navbar__logo" />
+                <h1 className="navbar__heading">AUTO CONCIERGE</h1> {/* New textual heading */}
+            </div>
             <div className="icon-burger" onClick={toggleMenu}>
                 <div className="line"></div>
                 <div className="line"></div>
                 <div className="line"></div>
-            </div>
-            <div className="navbar__logo">
-                {/* Your logo goes here */}
-                TRV Car Concierge
             </div>
         </nav>
     );
