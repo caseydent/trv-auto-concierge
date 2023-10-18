dce295708d8d8ae9b5a425eb12936589902ef3a8
Kiwicase <emailcaseydent@gmail.com>
2023-10-18 11:36:06 +0300

    Removed Hamburger icon

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,42 +1,16 @@
+// Navbar.js
 import './Navbar.css';
-import React, { useState } from 'react';
-import { motion } from 'framer-motion';
+import React from 'react';
 
 const Navbar = () => {
-    const [isOpen, setIsOpen] = useState(false);
-
     return (
         <nav className="navbar">
             <div className="navbar__logo">
+                {/* Your logo goes here */}
                 TRV Car Concierge
             </div>
-            <motion.div 
-                className="navbar__hamburger"
-                initial={{ opacity: 0 }}
-                animate={{ opacity: 1 }}
-                whileHover={{ scale: 1.1 }}
-                whileTap={{ scale: 0.9 }}
-                onClick={() => setIsOpen(!isOpen)}
-            >
-                <div className={`navbar__hamburger--lines ${isOpen ? 'open' : ''}`}>
-                    <span></span>
-                    <span></span>
-                    <span></span>
-                </div>
-            </motion.div>
-            {isOpen && (
-                <div className="navbar__menu">
-                    {/* Your menu items go here */}
-                </div>
-            )}
         </nav>
     );
 }
 
 export default Navbar;
-
-<div className="app">
-    <Navbar />
-    <img src="path_to_your_image.jpg" alt="Description of the image" className="header-image" />
-    {/* rest of your components or content */}
-</div>
