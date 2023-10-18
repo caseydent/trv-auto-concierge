1f56e332de3214230a43977b1e61d7fdb0f7e5eb
Kiwicase <emailcaseydent@gmail.com>
2023-10-18 12:30:06 +0300

    Hamburger icon and transformation implemented

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -3,8 +3,19 @@ import './Navbar.css';
 import React from 'react';
 
 const Navbar = () => {
+
+    function toggleMenu() {
+        const hamburgerMenu = document.querySelector('.icon-burger');
+        hamburgerMenu.classList.toggle('active');
+    }
+
     return (
         <nav className="navbar">
+            <div className="icon-burger" onClick={toggleMenu}>
+                <div className="line"></div>
+                <div className="line"></div>
+                <div className="line"></div>
+            </div>
             <div className="navbar__logo">
                 {/* Your logo goes here */}
                 TRV Car Concierge
