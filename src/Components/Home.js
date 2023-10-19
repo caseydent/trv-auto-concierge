27f4118c0dbfdce5df8d078ef52cde6e263b1868
Casey Dent - DealDash <casey@bidray.com>
2023-10-19 11:44:11 +0300

    Home.js and .css files added

---
+++ src/Components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Home.css';
+
+function Home() {
+  return (
+    <div className="home-section">
+      <img src="https://res.cloudinary.com/dqoibnakh/image/upload/v1697653393/ghscei35o2lourbtubg6.jpg" alt="Landrover" className="full-width-image" />
+      {/* Any other content for the Home section can be added here */}
+    </div>
+  );
+}
+
+export default Home;
