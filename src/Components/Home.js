adbc5c16a097bc1dafaf60e35fdf1cd37388c0b2
Casey Dent - DealDash <casey@bidray.com>
2023-10-19 12:19:25 +0300

    Home heading and subheading added

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -5,6 +5,10 @@ function Home() {
   return (
     <div className="home-section">
       <img src="https://res.cloudinary.com/dqoibnakh/image/upload/v1697653393/ghscei35o2lourbtubg6.jpg" alt="Landrover" className="full-width-image" />
+      <div className="home-text">
+        <h1 className="home-heading">HASSLE-FREE OWNERSHIP</h1>
+        <p className="home-subheading">We will handle it all - from dealership coordination to doorstep delivery</p>
+      </div>
       {/* Any other content for the Home section can be added here */}
     </div>
   );
