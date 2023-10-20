5e020e802f49a6149b78ef48fd81c94a115fa69e
Kiwicase <emailcaseydent@gmail.com>
2023-10-20 20:36:30 +0300

    Updated heading and subheading

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -6,8 +6,8 @@ function Home() {
     <div className="home-section">
       <img src="https://res.cloudinary.com/dqoibnakh/image/upload/v1697653393/ghscei35o2lourbtubg6.jpg" alt="Landrover" className="full-width-image" />
       <div className="home-text">
-        <h1 className="home-heading">HASSLE-FREE OWNERSHIP</h1>
-        <p className="home-subheading">We will handle it all - from dealership coordination to doorstep delivery</p>
+        <h1 className="home-heading">YOUR PERSONAL VEHICLE PROCUREMENT PARTNER</h1>
+        <p className="home-subheading">Simplifying the Journey<br></br>from Choice to Chassis</p>
       </div>
       {/* Any other content for the Home section can be added here */}
     </div>
