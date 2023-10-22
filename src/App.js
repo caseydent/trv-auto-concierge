361fba4fddb5dc28f8f013e5aa6b6d470a31999e
Kiwicase <emailcaseydent@gmail.com>
2023-10-22 15:37:59 +0300

    react-router-dom installed + Pages folder files

--- src/App.js
+++ src/App.js
@@ -1,15 +1,34 @@
 import './App.css';
 import Navbar from './Components/Navbar';
 import Home from './Pages/Home/Home';
+import HowItWorks from './Pages/HowItWorks/HowItWorks';
+import Services from './Pages/Services/Services';
+import AboutUs from './Pages/AboutUs/AboutUs';
+import Contact from './Pages/Contact/Contact';
+
+// Import necessary components from react-router-dom
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      {/* Rest of your content goes here */}
-      <Home /> {/* Use the Home component here */}
-      {/* Rest of your content goes here */}
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        {/* Routing logic starts here */}
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/how-it-works" component={HowItWorks} />
+          <Route path="/services" component={Services} />
+          <Route path="/about-us" component={AboutUs} />
+          <Route path="/contact" component={Contact} />
+        </Switch>
+        {/* Routing logic ends here */}
+      </div>
+    </Router>
   );
 }
 
