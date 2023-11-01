152092ebeeea2a5473ba158e0f16a8d9d52a3df7
caseydent <casey@bidray.com>
2023-11-01 12:14:04 +0200

    Testimonial updates

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -47,22 +47,10 @@ function Home() {
           </div>
         </div>
       </div>
+
       <div className="customer-showcase">
         <h2 className="showcase-title">Our Happy Customers</h2>
-
-        <div className="showcase-item">
-          <img
-            src="https://res.cloudinary.com/dqoibnakh/image/upload/v1698167117/Debbie_cauup3.png"
-            alt="Debbie Whalen"
-            className="full-width-image"
-          />
-          <h3 className="customer-name">Deanna Whalen - 2022 Ford Bronco</h3>
-          <p className="customer-testimonial">
-            "We were so excited, we couldn't resist sending you the picture and
-            would be thrilled if you share it. We still have the sign hanging
-            up!"
-          </p>
-        </div>
+        
         <div className="showcase-item">
           <img
             src="https://res.cloudinary.com/dqoibnakh/image/upload/v1698169178/A30600050_nonwgc.jpg"
@@ -70,12 +58,13 @@ function Home() {
             className="full-width-image"
           />
           <h3 className="customer-name">
-            Jimmy Cornelius - 2023 Bayliner Element M17 Boat
+            Jimmy - 2023 Bayliner Element M17 Boat
           </h3>
           <p>
             "Enjoying the Bayliner Element M17 on the water at Tampa Bay, FL"
           </p>
         </div>
+
         <div className="showcase-item">
           <img
             src="https://res.cloudinary.com/dqoibnakh/image/upload/v1698169607/A30688127_qmxpz1.jpg"
@@ -83,7 +72,7 @@ function Home() {
             className="full-width-image"
           />
           <h3 className="customer-name">
-            Christine Knatz - 2023 Yamaha Kodiak EPS 700 - Tactical Black
+            Christine - 2023 Yamaha Kodiak EPS 700 - Tactical Black
           </h3>
           <p className="customer-testimonial">
             "The 4 wheeler exceeded my expectations. From the handling to
@@ -93,15 +82,21 @@ function Home() {
             winch to remove trees in my way. up!"
           </p>
         </div>
+
         <div className="showcase-item">
           <img
             src="https://res.cloudinary.com/dqoibnakh/image/upload/v1698169935/Danny_fviind.png"
             alt="Danny Carr"
             className="full-width-image"
           />
-          <h3 className="customer-name">Danny Carr - 2022 Ford EcoSport S</h3>
-          {/* Add customer-testimonial here <p> */}
+          <h3 className="customer-name">Danny - 2022 Ford EcoSport S</h3>
+          <p className="customer-testimonial">
+            "I am very happy with my EcoSport...At the dealership, I gave 
+            them my drivers license and insurance card, they gave me my Key!
+            Easiest transaction ever"
+          </p>
         </div>
+
         <div className="showcase-item">
           <img
             src="https://res.cloudinary.com/dqoibnakh/image/upload/v1698170114/Jimmy_g4borv.png"
@@ -109,7 +104,7 @@ function Home() {
             className="full-width-image"
           />
           <h3 className="customer-name">
-            Jimmy Cornelius - 2023 Ford F-150 Lariat 4x4 5.0L V8 - Agate White
+            Jimmy - 2023 Ford F-150 Lariat 4x4 5.0L V8 - Agate White
             Metallic
           </h3>
           <p className="customer-testimonial">
@@ -117,6 +112,7 @@ function Home() {
           </p>
         </div>
         {/* Add more showcase items as needed */}
+
       </div>
       <div className="cta-section">
         <h2>Ready to find your perfect vehicle?</h2>
